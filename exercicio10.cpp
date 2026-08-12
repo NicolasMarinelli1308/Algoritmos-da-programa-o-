d93bff305f88fdd837c42aab21192c0af2871c17
Nicolas <itapenic33@gmail.com>
2026-08-12 20:15:22 -0300

    Remove exercicio10.cpp file

--- exercicio10.cpp
+++ exercicio10.cpp
@@ -1,11 +1,1 @@
-+ACM-include+ADw-stdio.h+AD4-
-+ACM-include+ADw-stdlib.h+AD4-
-	int main()+AHs-
-		int alunos +AD0- 45+ADs-
-		printf(+ACI-A quantidade de alunos eh +ACU-i+AFw-n+ACI-,alunos)+ADs-
-		
-		
-		
-		
-		Return 0+ADs-
-	+AH0-
+git rm exercicio10.cppp
